80865198f37ca60c7c135e29afffa58a7589337c
tung <22521619@gm.uit.edu.vn>
2024-05-12 04:09:37 +0700

    design private chat for werewolf

---
+++ Werewolf/Werewolf/Views/PrivateChat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Werewolf.Views
+{
+    public partial class PrivateChat : Form
+    {
+        public PrivateChat()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
