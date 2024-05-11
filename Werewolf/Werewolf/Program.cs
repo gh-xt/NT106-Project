8f21e744a66bf639b039df16eebdcde17e0c9e1c
tung <22521619@gm.uit.edu.vn>
2024-05-12 03:54:48 +0700

    set exe file

--- Werewolf/Werewolf/Program.cs
+++ Werewolf/Werewolf/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Werewolf.Views;
+
 namespace Werewolf
 {
     internal static class Program
@@ -16,7 +18,7 @@ namespace Werewolf
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form());
+            Application.Run(new MainWindow());
         }
     }
 }
