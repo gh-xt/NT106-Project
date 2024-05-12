2dd6d60188668ada5d98ea147aa2bcd34d1dac4d
MondayFeb <minhthu2nd.contact@gmail.com>
2024-05-12 14:41:56 +0700

    update

--- Werewolf/Werewolf/Views/GameView.Designer.cs
+++ Werewolf/Werewolf/Views/GameView.Designer.cs
@@ -31,9 +31,9 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameView));
             this.RoleList = new System.Windows.Forms.ListBox();
             this.UserList = new System.Windows.Forms.ListBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.ChatBox = new System.Windows.Forms.RichTextBox();
+            this.Message = new System.Windows.Forms.TextBox();
+            this.SendMessage = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // RoleList
@@ -52,43 +52,43 @@
             this.UserList.Size = new System.Drawing.Size(160, 446);
             this.UserList.TabIndex = 1;
             // 
-            // richTextBox1
+            // ChatBox
             // 
-            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox1.Location = new System.Drawing.Point(165, 30);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(431, 418);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = "";
+            this.ChatBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ChatBox.Location = new System.Drawing.Point(165, 30);
+            this.ChatBox.Name = "ChatBox";
+            this.ChatBox.Size = new System.Drawing.Size(431, 418);
+            this.ChatBox.TabIndex = 2;
+            this.ChatBox.Text = "";
             // 
-            // textBox1
+            // Message
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(-1, 469);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(597, 22);
-            this.textBox1.TabIndex = 3;
+            this.Message.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Message.Location = new System.Drawing.Point(-1, 469);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(597, 22);
+            this.Message.TabIndex = 3;
             // 
-            // button1
+            // SendMessage
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(603, 454);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(148, 38);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Gửi";
-            this.button1.UseVisualStyleBackColor = true;
+            this.SendMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SendMessage.Image = ((System.Drawing.Image)(resources.GetObject("SendMessage.Image")));
+            this.SendMessage.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.SendMessage.Location = new System.Drawing.Point(603, 454);
+            this.SendMessage.Name = "SendMessage";
+            this.SendMessage.Size = new System.Drawing.Size(148, 38);
+            this.SendMessage.TabIndex = 4;
+            this.SendMessage.Text = "Gửi";
+            this.SendMessage.UseVisualStyleBackColor = true;
             // 
             // GameView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(763, 501);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.SendMessage);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.ChatBox);
             this.Controls.Add(this.UserList);
             this.Controls.Add(this.RoleList);
             this.Name = "GameView";
@@ -102,8 +102,8 @@
 
         private System.Windows.Forms.ListBox RoleList;
         private System.Windows.Forms.ListBox UserList;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox ChatBox;
+        private System.Windows.Forms.TextBox Message;
+        private System.Windows.Forms.Button SendMessage;
     }
 }
